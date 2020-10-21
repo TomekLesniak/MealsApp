2294bf5788c174be1708d8a6fe151cd9e28dd84a
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-10-21 12:57:47 +0200

    Basic data

---
+++ lib/widgets/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:MealsApp/widgets/category_item.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES.map((categoryData) {
+          return CategoryItem(categoryData.title, categoryData.color);
+        }).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+      ),
+    );
+  }
+}
