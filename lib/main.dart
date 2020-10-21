17bf67354803b0916d8068ffb1c3d3c0ab1146d1
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-10-21 13:13:24 +0200

    Categories screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Meals App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                  fontFamily: 'RobotoCondensed',
+                  fontSize: 21,
+                  fontWeight: FontWeight.bold),
+            ),
       ),
       home: CategoriesScreen(),
     );
