edc192dec43b900063c461173b664dc3de630dcc
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-10-21 18:01:29 +0200

    Add NavBar

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,21 +6,17 @@ import '../dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meals'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES.map((categoryData) {
-          return CategoryItem(categoryData.id, categoryData.title, categoryData.color);
-        }).toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-      ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES.map((categoryData) {
+        return CategoryItem(
+            categoryData.id, categoryData.title, categoryData.color);
+      }).toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
     );
   }
 }
